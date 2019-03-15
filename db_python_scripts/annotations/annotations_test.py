31ee042f76e7e8348afd08bf019730ce4cfd8029
Jonathan Henderson <2252069h@bo720-10-18u.dcs.gla.ac.uk>
2019-03-15 21:21:56 +0000

    Converted annotations_test.py to a unit test

--- db_python_scripts/annotations/annotations_test.py
+++ db_python_scripts/annotations/annotations_test.py
@@ -1,42 +1,29 @@
 import boto3
+import unittest
 
-dynamodb = boto3.resource("dynamodb",endpoint_url="http://localhost:8000")
+dynamodb = boto3.resource("dynamodb")
+client = boto3.client('dynamodb')
 
 table = dynamodb.Table("annotations")
 
-table.put_item(
-    Item = {
-        'play_name' : 'Romeo',
-        'line_number' : 1,
-        'annotation' : "This is a test annotation"
-    }
-)
+class TestDatabaseCreation(unittest.TestCase):
+
+    def test_table_exists(self):
+        try:
+            table = dynamodb.Table("annotations")
+        except:
+            self.fail("Table was not created successfully")
+
+    def test_line_81(self):
+        response = table.get_item(Key = {'play_name':'Macbeth','line_number':81})
+        try:
+            item = response['Item']
+        except:
+            self.fail("Macbeth line 81 is not in the database")
+
+    def test_number_of_items(self):
+        response = client.describe_table(TableName='annotations')
+        self.assertEqual(response['Table']['ItemCount'],23)
 
 
-
-table.update_item(
-    Key ={
-        'play_name':'Romeo',
-        'line_number': 1
-    },
-    UpdateExpression='SET annotation = :val1',
-    ExpressionAttributeValues= {
-        ':val1':'This is new test annotation'
-    }
-)
-
-response = table.query{
-
-}
-
-items = response['Items']
-print(items)
-
-"""response = table.get_item(
-    Key = {
-        "play_name":'Romeo',
-        'line_number' : 1
-    }
-)
-item = response['Item']
-print(item)"""
+unittest.main()
